9ab73fe86ca85c185270c64d9299ab4f0410982c
Nikita Nechukhrin <nikita.nechukhrin@gmail.com>
2018-11-02 19:20:41 +0300

    Test of movement of the object and changing sprite during movement

--- sfmltest/main.cpp
+++ sfmltest/main.cpp
@@ -3,18 +3,19 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(640, 480), "Lesson 4", sf::Style::Default);
-	
+	sf::RenderWindow window(sf::VideoMode(640, 480), "Lesson 5", sf::Style::Default);
+
+	//Get Image, texture & sprite
 	sf::Image heroimage;
 	heroimage.loadFromFile("images/hero.png");
-	
 	sf::Texture herotexture;
 	herotexture.loadFromImage(heroimage);
 	
 	sf::Sprite herosprite;
 	herosprite.setTexture(herotexture);
-	herosprite.setTextureRect(sf::IntRect(0, 192, 96, 96));
-	herosprite.setPosition(50, 25);
+	herosprite.setTextureRect(sf::IntRect(0, 192, 96, 96)); 
+	herosprite.setPosition(250, 250);
+
 
 	while (window.isOpen())
 	{
@@ -24,6 +25,13 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+		
+		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || (sf::Keyboard::isKeyPressed(sf::Keyboard::A)))) { herosprite.move(-0.1, 0); herosprite.setTextureRect(sf::IntRect(0, 96, 96, 96)); }
+		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || (sf::Keyboard::isKeyPressed(sf::Keyboard::D)))) { herosprite.move(0.1, 0); herosprite.setTextureRect(sf::IntRect(0, 192, 96, 96)); }
+		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || (sf::Keyboard::isKeyPressed(sf::Keyboard::W)))) { herosprite.move(0, -0.1); herosprite.setTextureRect(sf::IntRect(0, 288, 96, 96)); }
+		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || (sf::Keyboard::isKeyPressed(sf::Keyboard::S)))) { herosprite.move(0, 0.1); herosprite.setTextureRect(sf::IntRect(0, 0, 96, 96)); }
+		
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) herosprite.setColor(sf::Color::Red);
 		
 		window.clear();
 		window.draw(herosprite);
