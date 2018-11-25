ad698c893f3ae68a97ba52c3a1daa567aabfecf0
Nikita Nechukhrin <nikita.nechukhrin@gmail.com>
2018-11-25 04:17:21 +0300

    Fixes

--- sfmltest/map.h
+++ sfmltest/map.h
@@ -2,8 +2,8 @@
 #define MAP
 #include <SFML\Graphics.hpp>
 
-const int HEIGHT_MAP = 25;
-const int WIDTH_MAP = 25;
+const int HEIGHT_MAP = 15;
+const int WIDTH_MAP = 20;
 
 extern sf::String TileMap[HEIGHT_MAP];
 
